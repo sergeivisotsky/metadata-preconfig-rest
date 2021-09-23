e5e2541c2b701652d935a87cf8879a4c12edd5ad
Sergei Visotsky <sergei.visotsky@gmail.com>
2021-09-23 10:58:06 +0200

    provide navigation metadata (#2)

---
+++ metadata-provider-app/src/main/java/io/github/sergeivisotsky/metadata/preconfig/app/dto/ExtendedNavigationElement.java
@@ -0,0 +1,9 @@
+package io.github.sergeivisotsky.metadata.preconfig.app.dto;
+
+import io.github.sergeivisotsky.metadata.selector.dto.NavigationElement;
+
+/**
+ * @author Sergei Visotsky
+ */
+public class ExtendedNavigationElement extends NavigationElement {
+}
