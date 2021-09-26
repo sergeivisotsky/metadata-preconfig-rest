5b8d7b5c41a572d42eacee5ebe202ea47f98df07
Sergei Visotsky <sergei.visotsky@gmail.com>
2021-09-26 19:59:23 +0200

    hierarchical list helper impl in preconfig

---
+++ metadata-provider-app/src/main/java/io/github/sergeivisotsky/metadata/preconfig/app/dto/ExtendedLookupInfo.java
@@ -0,0 +1,6 @@
+package io.github.sergeivisotsky.metadata.preconfig.app.dto;
+
+import io.github.sergeivisotsky.metadata.selector.dto.form.LookupInfo;
+
+public class ExtendedLookupInfo extends LookupInfo {
+}
--- metadata-provider-app/src/main/java/io/github/sergeivisotsky/metadata/preconfig/app/mapper/FormSectionMapper.java
+++ metadata-provider-app/src/main/java/io/github/sergeivisotsky/metadata/preconfig/app/mapper/FormSectionMapper.java
@@ -12,13 +12,16 @@ public class FormSectionMapper implements MetadataMapper<FormSection> {
 
     @Override
     public String getSql() {
-        return "SELECT fs.name,\n" +
-                "       fs.parent_section_name,\n" +
+        return "SELECT fs.id,\n" +
+                "       fs.name,\n" +
+                "       ParentSection.name AS parent_section_name,\n" +
                 "       fs.cardinality,\n" +
+                "       fs.order_in_form,\n" +
                 "       tr.ui_name,\n" +
                 "       tr.ui_description\n" +
                 "FROM form_section fs\n" +
-                "         LEFT JOIN amd_translation tr on tr.form_name = fs.name\n" +
+                "         LEFT JOIN form_section ParentSection ON fs.id = ParentSection.parent_section_id\n" +
+                "         JOIN amd_translation tr ON tr.form_name = fs.name\n" +
                 "WHERE fs.name = :formName\n" +
                 "  AND tr.lang = :lang";
     }
@@ -29,6 +32,7 @@ public class FormSectionMapper implements MetadataMapper<FormSection> {
             ExtendedFormSection formSection = new ExtendedFormSection();
             formSection.setName(rs.getString("name"));
             formSection.setParentSectionName(rs.getString("parent_section_name"));
+            formSection.setOrderInForm(rs.getInt("order_in_form"));
             formSection.setCardinality(FormSectionCardinality.valueOf(rs.getString("cardinality")));
             formSection.setUiName(rs.getString("ui_name"));
             formSection.setUiDescription(rs.getString("ui_description"));
