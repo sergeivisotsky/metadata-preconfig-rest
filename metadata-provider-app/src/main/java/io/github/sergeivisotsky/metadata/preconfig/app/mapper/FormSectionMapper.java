aa79501c7143fa41a2146d0e9428429f09407d1d
Sergei Visotsky <sergei.visotsky@gmail.com>
2021-09-29 14:48:44 +0200

    get rid of hierarchy

--- metadata-provider-app/src/main/java/io/github/sergeivisotsky/metadata/preconfig/app/mapper/FormSectionMapper.java
+++ metadata-provider-app/src/main/java/io/github/sergeivisotsky/metadata/preconfig/app/mapper/FormSectionMapper.java
@@ -14,13 +14,11 @@ public class FormSectionMapper implements MetadataMapper<FormSection> {
     public String getSql() {
         return "SELECT fs.id,\n" +
                 "       fs.name,\n" +
-                "       ParentSection.name AS parent_section_name,\n" +
                 "       fs.cardinality,\n" +
                 "       fs.order_in_form,\n" +
                 "       tr.ui_name,\n" +
                 "       tr.ui_description\n" +
                 "FROM form_section fs\n" +
-                "         LEFT JOIN form_section ParentSection ON fs.id = ParentSection.parent_section_id\n" +
                 "         JOIN amd_translation tr ON tr.form_name = fs.name\n" +
                 "WHERE fs.name = :formName\n" +
                 "  AND tr.lang = :lang";
@@ -31,7 +29,6 @@ public class FormSectionMapper implements MetadataMapper<FormSection> {
         try {
             ExtendedFormSection formSection = new ExtendedFormSection();
             formSection.setName(rs.getString("name"));
-            formSection.setParentSectionName(rs.getString("parent_section_name"));
             formSection.setOrderInForm(rs.getInt("order_in_form"));
             formSection.setCardinality(FormSectionCardinality.valueOf(rs.getString("cardinality")));
             formSection.setUiName(rs.getString("ui_name"));
